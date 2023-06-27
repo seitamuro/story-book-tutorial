fad630ceaf89cbee8327368f103236643873f6f5
seitamuro <seitamuro0210@gmail.com>
2023-06-28 07:29:48 +0900

    [add] accesibility追加

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   stories: ['../src/components/**/*.stories.js'],
   staticDirs: ['../public'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app', '@storybook/addon-interactions'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app', '@storybook/addon-interactions', "@storybook/addon-a11y", "@storybook/addon-viewport"],
   features: {
     postcss: false
   },
